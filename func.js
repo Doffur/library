1e3b8dc5d77b0ec2b249995435e94ee4f00a17eb
Samir Ahajin <samirahajin@gmail.com>
2021-12-01 13:40:28 +0800

    added the functionality and design, able to add and remove a book

--- func.js
+++ func.js
@@ -1,6 +1,17 @@
 let myLibrary =[];
 let bookCollection = document.querySelector('.books');
 let myForm = document.querySelector('form');
+let buttonVal = 1;
+let BookDef = new Book()
+    BookDef.bookNumber = 1;
+    BookDef.title = "Harry Potter and the Philosopher's Stone";
+    BookDef.author = "J.K. Rowling";
+    BookDef.pages = "223";
+    BookDef.read = "notyet";
+    myLibrary.push(BookDef);
+    displayBook(BookDef);
+
+
 
 function Book(){
     this.bookNumber;
@@ -10,23 +21,25 @@ function Book(){
     this.read;
 }
 
-
-
 function addBookToLibrary(){
     let bAuthor = document.getElementById('author').value;
     let bTitle = document.getElementById('title').value;
     let bPages= document.getElementById('pages').value;
     let bRead = document.querySelector('.checkBox:checked').value;
     let cBook= new Book();
-    cBook.bookNumber = myLibrary.length+1;
-    cBook.title = bTitle;
-    cBook.author =bAuthor;
-    cBook.pages = bPages;
-    cBook.read = bRead;
-    myLibrary.push(cBook);
-    //getting the value inside the array myLibrary[arrayvalue].key
-    displayBook(cBook)
-    //myForm.reset();//reset the input field
+    if(!([bAuthor.length,bTitle.length,bPages.length,bRead.length].includes(0))){
+        cBook.bookNumber = myLibrary.length+1;
+        cBook.title = bTitle;
+        cBook.author =bAuthor;
+        cBook.pages = bPages;
+        cBook.read = bRead;
+        myLibrary.push(cBook);
+        //getting the value inside the array myLibrary[arrayvalue].key
+        displayBook(cBook);
+        let check = document.querySelectorAll('.checkBox');
+        check.forEach(a=> a.disabled = false);
+        myForm.reset();//reset the input field
+    }
 }
 
 function disableOther(c){//disable other checkbox if the other is checked
@@ -35,36 +48,41 @@ function disableOther(c){//disable other checkbox if the other is checked
         b.disabled = !(!c.checked || b === c);
     } 
 }
-let buttonVal = 1;
+
+
 function displayBook(curBook){
     let cardVal = document.createElement("div");
-    let remove = document.createElement("button"); 
+    
     //add details in card
     let stat = ['Title', 'Author','Pages']
     let counter = 0;
     for(const key in curBook){
         let item = document.createElement("div"); 
+        let title = document.createElement("div"); 
+
         if(`${key}`== "bookNumber"){
-            console.log("hello");
             continue;
         }else if(['read','notyet'].includes(`${curBook[key]}`)){
             let buttonRead = document.createElement('button'); 
-            buttonRead.classList.add("read"); 
-            //buttonRead.setAttribute("id",buttonVal);
+            let remove = document.createElement("button"); 
             let word;
+            buttonRead.classList.add("read"); 
             `${curBook[key]}`=='read'? word = "Already Read":word = "Didn\'t Read Yet";
             buttonRead.textContent = word;
-            cardVal.appendChild(buttonRead);
+            remove.textContent = "Remove";
+            remove.classList.add("remove");
+            item.setAttribute("id",buttonVal); 
+            item.classList.add("buttons");
+            item.appendChild(buttonRead);
+            item.appendChild(remove);
+            cardVal.appendChild(item);
         }else{
-        item.textContent = `${stat[counter]} : ${curBook[key]}`;
+       //bold the title page author 
+        item.innerHTML =`<strong>${stat[counter]}</strong>:${curBook[key]}`;
         cardVal.appendChild(item);
         }
         counter++;
     } 
-    remove.textContent = "Remove";
-    remove.classList.add("remove");
-    remove.setAttribute("id",buttonVal); 
-    cardVal.appendChild(remove);
     cardVal.classList.add('card');
     cardVal.setAttribute("id","card"+buttonVal); 
     bookCollection.appendChild(cardVal);
@@ -78,30 +96,34 @@ let modal = document.getElementById('modal');
 
 
 addBook.addEventListener('click',addBookToLibrary);
+
+
+
 openModal.addEventListener('click',function(){
     modal.style.display = 'block';
 })
+
 closeModal.addEventListener('click',function(){
     modal.style.display = 'none';
 })
 
 window.addEventListener('click',function(e){
-
-
     if(e.target == modal){
         modal.style.display = 'none';
     }else if(e.target.classList.value == 'read'){
-        //use below if e doesnt work
-        //let button = document.querySelector(`.read[id = "${e.target.id}"]`);
-        e.target.textContent == "Already Read"?e.target.textContent = "Didn\'t Read Yet":e.target.textContent = "Already Read";     
-    }else if(e.target.classList.value == 'remove'){
-        let curCard = document.querySelector(`[id=card${e.target.id}]`)
-        myLibrary.splice(myLibrary.findIndex(a=>a.bookNumber == e.target.id),1);
-        bookCollection.removeChild(curCard);
-        console.log(myLibrary);
-        console.log(bookCollection);
-
+        if( e.target.textContent == "Already Read"){
+            e.target.textContent = "Didn\'t Read Yet"
+            myLibrary[e.target.parentNode.id-1].read = "notyet";
+        }else if(e.target.textContent == "Didn\'t Read Yet"){
+            e.target.textContent = "Already Read"
+            myLibrary[e.target.parentNode.id-1].read = "read";
+        }
        
+    }else if(e.target.classList.value == 'remove'){  
+        let curCard = document.querySelector(`[id=card${e.target.parentNode.id}]`)
+        myLibrary.splice(myLibrary.findIndex(a=>a.bookNumber == e.target.parentNode.id),1);
+        bookCollection.removeChild(curCard);
+
     }
     
 })
